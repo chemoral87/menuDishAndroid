82abd48bd8d9b64551f426ba4ce8f2de354f43c6
sergio <chemoral87@hotmail.com>
2021-07-01 18:00:37 -0500

    curse 23

--- app/src/main/java/com/example/menu/view/fragments/FavoriteDishesFragment.kt
+++ app/src/main/java/com/example/menu/view/fragments/FavoriteDishesFragment.kt
@@ -8,13 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.menu.databinding.FragmentDashboardBinding
+
+import com.example.menu.databinding.FragmentFavoriteDishesBinding
 import com.example.menu.viewmodel.DashboardViewModel
 
-class DashboardFragment : Fragment() {
+class FavoriteDishesFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFavoriteDishesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +29,7 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteDishesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
